11245ae8d0258a67e79e449be8a4ec040d33e2b9
Andy Wright <atwright147@gmail.com>
2022-05-01 22:51:38 +0100

    fix issues with env types

--- environment.d.ts
+++ environment.d.ts
@@ -1,5 +1,7 @@
 declare namespace NodeJS {
   interface ProcessEnv {
+    DB_FILE: string;
+    DB_PATH: string;
     NODE_ENV: 'development' | 'production';
     PORT: string;
     SECRET: string;
--- knexfile.ts
+++ knexfile.ts
@@ -6,16 +6,14 @@ require('ts-node/register');
 import path from 'path';
 import { Knex } from 'knex';
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const dbFolder = path.resolve(process.env.DB_PATH!);
+const dbFolder = path.resolve(process.env.DB_PATH);
 
 const knexConfig: Record<string, Knex.Config> = {
   development: {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      filename: path.join(dbFolder, process.env.DB_FILE!),
+      filename: path.join(dbFolder, process.env.DB_FILE),
     },
     migrations: {
       directory: path.join(dbFolder, 'migrations'),
